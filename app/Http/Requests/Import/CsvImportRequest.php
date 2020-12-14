631a3b4877da443e9fe2273c03701b6fad30575d
DESKTOP-6GUFCUQ\Jelena <jelence1994@hotmail.rs>
2020-12-14 04:25:43 +0100

    added authorize fucntion to CsvImportRequest

--- app/Http/Requests/Import/CsvImportRequest.php
+++ app/Http/Requests/Import/CsvImportRequest.php
@@ -3,12 +3,18 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class CsvImportRequest extends FormRequest
 {
     public const FILE_TYPE_CSV = "csv_file";
 
-    public function rules()
+    public function authorize(): bool
+    {
+        return Auth::user() !== null;
+    }
+
+    public function rules(): array
     {
         return [
             self::FILE_TYPE_CSV => "required|file"
