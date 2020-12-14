5b83038471aa0895fe6dd3867618e9cc39817b5f
DESKTOP-6GUFCUQ\Jelena <jelence1994@hotmail.rs>
2020-12-14 14:11:50 +0100

    commented unit tests for authentication for now

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model implements CustomerInterface
 {
+    public const VALIDATION_RULES = [
+        "full_name" => ["required"],
+        "goal" => ["in:"],
+        "age" => ["required", "integer"]
+    ];
+
     protected $guarded = [];
 
     public function customerStrengths()
